42508dd2a9cce26eed9019ce8915504ff5168b77
ramawahyu112 <52642373+ramawahyu112@users.noreply.github.com>
2022-06-12 22:35:36 +0800

    commit promise

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let theaterIxx = await promiseTheaterIXX();
+  let theaterVgc = await promiseTheaterVGC();
+  let allTheater = [...theaterIxx, ...theaterVgc];
+
+  let jumlahEmosi = 0;
+  allTheater.map((e) => (e.hasil == emosi ? (jumlahEmosi += 1) : null));
+
+  return jumlahEmosi;
+};
 
 module.exports = {
   promiseOutput,
